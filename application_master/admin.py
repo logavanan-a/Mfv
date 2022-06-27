10acb6840f9a990f468c52bb873027f154681be2
Akash TS <akash.t.s@mahiti.org>
2022-06-27 14:25:31 +0530

    Created models in application master app

--- application_master/admin.py
+++ application_master/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(State)
+admin.site.register(District)
+admin.site.register(Partner)
+admin.site.register(Mission)
+admin.site.register(PartnerMissionMapping)
+admin.site.register(Donor)
+
--- application_master/models.py
+++ application_master/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class BaseContent(models.Model):
+    ACTIVE_CHOICES = ((0, 'Deleted'), (2, 'Active'),(3,"Inactive"))
+    active          = models.PositiveIntegerField(choices=ACTIVE_CHOICES,default=2)
+    created         = models.DateTimeField(auto_now_add=True)
+    modified        = models.DateTimeField(auto_now=True)
+    listing_order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        abstract    = True
+
+class State(BaseContent):
+    name = models.CharField(max_length=50)
+
+class District(BaseContent):
+    name = models.CharField(max_length=50)
+    state =  models.ForeignKey(State, on_delete = models.DO_NOTHING)
+
+class Partner(BaseContent):
+    name = models.CharField(max_length=50)
+    district =  models.ForeignKey(District, on_delete = models.DO_NOTHING)
+
+class Mission(BaseContent):
+    name = models.CharField(max_length=50)
+
+class PartnerMissionMapping(BaseContent):
+    partner = models.ForeignKey(Partner, on_delete = models.DO_NOTHING)
+    mission = models.ForeignKey(Mission, on_delete = models.DO_NOTHING)
+
+class Donor(BaseContent):
+    name = models.CharField(max_length=50)
+    logo = models.ImageField(upload_to='image_folder/')
+    Date_of_association = models.DateField()
+
+
+
