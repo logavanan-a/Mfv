90003a0ac4ae47fb4edfd6d679f04fb6c5f7822d
Akash TS <akash.t.s@mahiti.org>
2022-08-24 16:18:44 +0530

    edit show

--- mis/admin.py
+++ mis/admin.py
@@ -8,7 +8,7 @@ from mis.models import MissionIndicatorAchievement, Task
 class AdminMissionIndicatorAchievement(ImportExportActionModelAdmin,admin.ModelAdmin):
     exclude = ['response']
     search_fields = ['task__name']
-    list_display = ('id', 'active', 'created', 'modified', 'listing_order', 'task', 'number_working_days', 'project_reference_file')
+    list_display = ('id', 'active', 'created', 'modified', 'listing_order', 'task', 'number_working_days', 'project_reference_file', 'camp_organized')
     # def get_list_display(self, request):
     #     return [field.name for field in self.model._meta.concrete_fields]
 
