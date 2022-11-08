db7ab8656b50e3777d3f97880cfd03f3c1dbf0fb
yuvarajkharvi <yuvarajkharvi726@gmail,com>
2022-11-08 20:45:39 +0530

    dashboard_queries updated the date storing

--- dashboard/management/commands/dashboard_queries.py
+++ dashboard/management/commands/dashboard_queries.py
@@ -4,25 +4,28 @@ import sys
 import traceback
 from dashboard.models import DashboardSummaryLog
 from datetime import datetime
+from django.utils import timezone
+
 # import logging
 import time
 
-
+#Cron tab will set for every hour 20th and 40 th minutes
 class Command(BaseCommand):
-    help = 'Run command to refresh materialized views concurrently'
+    help = 'Run command to refresh materialized views'
 
     def handle(self, *args, **options):
         import datetime
         # logger = logging.getLogger('dashboard')
         refresh_query_list = {"mat_partner_mission_meta_view":"refresh materialized view mat_partner_mission_meta_view", 
                       "mat_target_view":"refresh materialized view mat_target_view", 
-                      "mat_dashboard_achievement_view":"refresh materialized view mat_dashboard_achievement_view"}
+                      "mat_dashboard_achievement_view":"refresh materialized view mat_dashboard_achievement_view",
+                      "mat_achievement_view":"refresh materialized view mat_achievement_view"}
         for key,query in refresh_query_list.items():
             with connection.cursor() as cursor:
                 try:
                     cursor.execute(query)
                     # cursor.fetchall()
-                    now = datetime.datetime.now()
+                    now = timezone.localtime(timezone.now())
                     logdata, created = DashboardSummaryLog.objects.get_or_create(log_key=key)
                     logdata.last_successful_update = now
                     logdata.most_recent_update = now
@@ -37,7 +40,7 @@ class Command(BaseCommand):
                     # logger.error(error_stack)
                     logdata, created = DashboardSummaryLog.objects.get_or_create(log_key=key)
                     logdata.most_recent_update_status = 'Failed'
-                    logdata.most_recent_update = datetime.datetime.now()
+                    logdata.most_recent_update = timezone.localtime(timezone.now())
                     logdata.error_details = str(error_stack)
                     logdata.save()
             time.sleep(1)
